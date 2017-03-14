09fc8041d8eae74d446c532189f95ae4dd592726
Lee Wonhee <orycons@gmail.com>
2017-03-14 11:46:02 +0900

    문자열 연습1

---
+++ 문자열.py
@@ -0,0 +1,11 @@
+a = "he's a doctor."
+print(a)
+
+food = "Python's favorite food is perl."
+print(food)
+
+say = "\"Python is very easy.\" he says."
+print(say)
+
+multiline = "Life is too short\nYou need python"
+print(multiline)
