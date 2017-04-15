7edd0a13459814f7c2448888c4bc026db12967eb
Lee Wonhee <orycons@gmail.com>
2017-04-15 20:56:36 +0900

    ㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁ

---
+++ s.py
@@ -0,0 +1,5 @@
+Treehit = 0
+while Treehit < 10:
+    Treehit+=1
+    print("나무를 %d번 찍었습니다." % (Treehit))
+    wdd
--- ㄴ.py
+++ ㄴ.py
@@ -1,5 +1,1 @@
-def add(a, b):
-    print("ADDING %d + %d" % (a, b))
-    return a - b
-a = add(1,2)
-print(a)
+
